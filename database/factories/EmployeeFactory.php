cb2bdc7573708e34c910309f48d43389e79b534e
SPerepelitsa <sergei.poltava@gmail.com>
2019-02-16 20:27:18 +0200

    EmployeeFactory for seeding created

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Employee::class, function (Faker $faker) {
+    return [
+        'name' => $faker->firstNameMale,
+        'middle_name' => $faker->firstNameMale,
+        'lastname' => $faker->lastName,
+        'position' => $faker->jobTitle,
+        'hire_date' => $faker->dateTime,
+        'salary' => $faker->numberBetween($min = 3000, $max = 15000),
+        'manager_id' => $faker->numberBetween($min = 1, $max = 5)
+
+    ];
+});
